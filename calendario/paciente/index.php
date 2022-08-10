a724759b595bd66c91f91d1f9757936840d41c0b
hideky24 <88519929+hideky24@users.noreply.github.com>
2022-08-09 23:14:15 -0500

    Add files via upload

--- calendario/paciente/index.php
+++ calendario/paciente/index.php
@@ -15,22 +15,23 @@ $data = $p->getALL();
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
-        <h2 class="text-center mb-2">Calendario </h2>
-        <div class="row">
+<div class="container" >
+        <h2 class="text-center mb-2" >Calendario </h2>
+        <div class="row" >
             
     <?php_
-while ( $pt = mysqli_fetch_object(data) ) {
+while ($pt = mysqli_fetch_object($data)) {
  $date =$pt-> fecha;
- echo "<div class='col'>";
- echo "<div>";
- echo "<h5><img src='".ROOT."/images/$pt->imagen' width='50' height='50'/>$pt->nombres $pt->apellidos </h5>;"
- echo "<p> <b>Fecha:</b> ".date('D', strtotime($date))." ". date('M-Y H:i',strtotime($date)) ." </p>";
- echo  "<div class='text-center'>";
- echo "<a class='btn btn-success' href='#'>Modificar</a> - <a class='btn btn-danger ' href='#'>Eliminar </a>"; 
+ echo "<div class='col' >";
+ echo "<div class='border border-infor p-2 > ";
+ echo "<h5><img src='".ROOT."/images/$pt->imagen' width='50' height='50'/>$pt->nombres 
+ $pt->apellidos </h5>";
+ echo " <p> <b>Fecha:</b> ".date('D', strtotime($date) )." ".date('M-Y H:i',strtotime($date)). " 
+ </p>";
+ echo  " <div class='text-center'> ";
+ echo "<a class='btn btn-success' href='edit.php' >Modificar </a> - <a class='btn btn-danger ' href='#'>Eliminar </a>"; 
  echo  "</div>";
  echo "</div>";
-
  echo "</div>";
  
 }
